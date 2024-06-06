fa8b20c93f00cca6351d086bca8fcd33dd186480
Morne Vermeulen <morne@redvine.io>
2024-06-06 12:29:27 +0200

    latest

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import numbers
 import json
 import requests
 
+api_endpoint = "https://saffaicecream.co.za/login.php"
+
 authorisation = {
     "username": "morne@redvine.io",
     "password": "testing-ice-cream-api",
-    "endpoint": "https://saffaicecream.co.za/login.php",
     "authorization": {
         "admin": True,
         "superuser": True
     }
 }
+
+response = requests.get(api_endpoint,headers=authorisation)
