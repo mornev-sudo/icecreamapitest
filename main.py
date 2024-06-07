7f78e6a6e9f17bfdc252ffc51d32729552a0485f
Morne Vermeulen <morne@redvine.io>
2024-06-07 06:24:46 +0200

    added api key

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import json
 import requests
 
 api_endpoint = "https://saffaicecream.co.za/login.php"
+api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlblV1aWQiOiJlZTZhNjE1OC0zMjExLTRiZWYtOWM4Yi02ITU1OWJkYWExODciLCJleHAiOjE3NDkyNzAwOTgsInV1aWQiOiI5ThiSisNoTAR3alAPIt0k3nzAzOWQiLCJpYXQiOjE3MTc3MzQxMzR9.rXEuI5KVeQbYml4MYhBde5PPDtl4hD--WYGMNLua4n8"
 
-authorisation = {
+headers = {
     "username": "morne@redvine.io",
     "password": "testing-ice-cream-api",
     "authorization": {
@@ -13,4 +14,7 @@ authorisation = {
     }
 }
 
-response = requests.get(api_endpoint,headers=authorisation)
+authorisation = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlblV1aWQiOiJlZTZhNjE1OC0zMjExLTRiZWYtOWM4Yi02ITU1OWJkYWExODciLCJleHAiOjE3NDkyNzAwOTgsInV1aWQiOiI5ThiSisNoTAR3alAPIt0k3nzAzOWQiLCJpYXQiOjE3MTc3MzQxMzR9.rXEuI5KVeQbYml4MYhBde5PPDtl4hD--WYGMNLua4n8"
+
+response = requests.get(api_endpoint,headers=authorisation, authorization=authorisation)
+
